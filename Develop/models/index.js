d1ba221978160e34cd923af7062e618e70d97269
Nathan Nguyen <nathanknguyen96@gmail.com>
2021-08-19 19:51:23 -0700

    relating all models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -3,14 +3,37 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { belongsToMany } = require('./Product');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  // Define the third table needed to store the foreign keys
+  foreignKey: "category_id"
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  // Define the third table needed to store the foreign keys
+  foreignKey: "category_id",
+  onDelete: "CASCADE"
+});
+
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag
+  }
+})
+
+
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag
+  }
+})
 
 module.exports = {
   Product,
